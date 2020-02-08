0a4130598a98d0c5a7eae01cf703546b96cd57d0
sridharke7 <sridhar.kesavan@outlook.com>
2020-02-09 00:08:48 +0900

    Initial Commit

---
+++ src/data/cats.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'tuxedo',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweet kitty, He loves getting his belly rubbed.'
+  },
+  {
+    name: 'Henry',
+    breed: 'tabby',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'orange/white',
+    weight: 17,
+    location: 'threed',
+    notes: 'Super friendly.'
+  },
+  {
+    name: 'Roger',
+    breed: 'tabby',
+    species: 'cat',
+    gender: 'male',
+    age: 20,
+    color: 'grey',
+    weight: 15,
+    location: 'threeed',
+    notes: 'Pure breed. Trained for competitions.'
+  }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    species: 'dog',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Hillary',
+    breed: 'mut',
+    species: 'dog',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'threed',
+    notes: 'Super friendly.'
+  },
+  {
+    name: 'Zeus',
+    breed: 'afghan hound',
+    species: 'dog',
+    gender: 'male',
+    age: 9,
+    color: 'grey',
+    weight: 68,
+    location: 'threeed',
+    notes: 'Pure breed. Trained for competitions.'
+  }
+]
